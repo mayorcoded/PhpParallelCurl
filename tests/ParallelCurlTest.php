f4d8fd202e38379bdaaccec4a857f0f48e6caee0
Mayowa <mtudonu85@gmail.com>
2017-12-23 19:09:58 +0100

    Add test components for class

---
+++ tests/ParallelCurlTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 12/23/17
+ * Time: 6:32 PM
+ */
+
+use mayorcoded\PhpParallelCurl\ParallelCurl;
+class ParallelCurlTest extends PHPUnit\Framework\TestCase
+{
+    public function testExecuteMethod(){
+        $parallelCurl = new ParallelCurl();
+        $this->assertTrue(is_object($parallelCurl));
+        unset($var);
+    }
+
+    public function testExecuteMethod2(){
+        $parallelCurl = new ParallelCurl();
+        $this->assertTrue($parallelCurl->execute() == "Hey");
+        unset($var);
+    }
+}
